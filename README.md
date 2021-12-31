0e520cb32a8da5f1bd38ea249a7765748e460675
Zhuxian206 <linz206676@gmail.com>
2021-12-31 11:29:04 +0800

    說明檔更新

--- README.md
+++ README.md
@@ -1,23 +1,30 @@
-movieSearch
 # 電影查詢
 
 
-資料來源
+## 資料來源
+網頁資料
 https://www.agentm.tw/
 
-圖片來源
+使用圖片來源
 https://unsplash.com/photos/dWYjy9zIiF8
 https://unsplash.com/photos/nW1n9eNHOsc
 
 ## 使用說明
 機器人有提供「電影搜尋」、「新片推薦」、「查看周末票房名次」等功能。
-如有想要搜尋的電影，可直接手動輸入片名關鍵字。
 
+### 如有想要搜尋的電影，可直接手動輸入關鍵字<br />點選按鈕會跳至電影資料頁
+<img src="https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true" style="width: 300px"> <img src="https://github.com/Zhuxian206/movieSearch/blob/master/images/Screenshot_20211231-095723.jpg?raw=true" style="width: 300px">
+
+### 代號輸入與按鈕
 輸入「!new」，會隨機向您推薦五部本周新片。
 輸入「!p」，則會顯示上周末的票房名單。
 想查看使用方式請輸入「!說明」。
 
 也可點選畫面下方選單使用以上功能。
+<img src="https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true" style="width: 300px">
+
+### 點選票房清單中的片名欄位，也會跳轉至電影資料頁
+<img src="https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true" style="width: 300px"> <img src="https://github.com/Zhuxian206/movieSearch/blob/master/images/Screenshot_20211231-095752.jpg?raw=true" style="width: 300px">
 
 ## 掃描 QR code
 機器人好友ID：@559fjfsz
--- commands/instruction.js
+++ commands/instruction.js
@@ -1,30 +1,5 @@
 export default async (event) => {
   try {
-    // const ins = {
-    //   type: 'flex',
-    //   altText: '電影搜尋結果',
-    //   contents: {
-    //     type: 'carousel',
-    //     contents: [
-    //       {
-    //         type: 'image',
-    //         originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true',
-    //         previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true'
-    //       },
-    //       {
-    //         type: 'image',
-    //         originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true',
-    //         previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true'
-    //       },
-    //       {
-    //         type: 'image',
-    //         originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true',
-    //         previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true'
-    //       }
-    //     ]
-    //   }
-    // }
-
     const ins = [
       {
         type: 'image',
