723462c31fc8470bb3e40af84d4746bbae5e48f2
Zhuxian206 <linz206676@gmail.com>
2021-12-31 10:54:10 +0800

    說明按鈕調整

--- commands/instruction.js
+++ commands/instruction.js
@@ -1,29 +1,47 @@
 export default async (event) => {
   try {
-    const ins = {
-      type: 'flex',
-      altText: '電影搜尋結果',
-      contents: {
-        type: 'carousel',
-        contents: [
-          {
-            type: 'image',
-            originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true',
-            previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true'
-          },
-          {
-            type: 'image',
-            originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true',
-            previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true'
-          },
-          {
-            type: 'image',
-            originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true',
-            previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true'
-          }
-        ]
+    // const ins = {
+    //   type: 'flex',
+    //   altText: '電影搜尋結果',
+    //   contents: {
+    //     type: 'carousel',
+    //     contents: [
+    //       {
+    //         type: 'image',
+    //         originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true',
+    //         previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true'
+    //       },
+    //       {
+    //         type: 'image',
+    //         originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true',
+    //         previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true'
+    //       },
+    //       {
+    //         type: 'image',
+    //         originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true',
+    //         previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true'
+    //       }
+    //     ]
+    //   }
+    // }
+
+    const ins = [
+      {
+        type: 'image',
+        originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true',
+        previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true'
+      },
+      {
+        type: 'image',
+        originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true',
+        previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true'
+      },
+      {
+        type: 'image',
+        originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true',
+        previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true'
       }
-    }
+    ]
     event.reply(ins)
   } catch (error) {
     console.log(error)
