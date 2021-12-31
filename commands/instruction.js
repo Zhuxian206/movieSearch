031d516024ff800dd33b25c89f8e663f68146bbf
Zhuxian206 <linz206676@gmail.com>
2021-12-31 10:49:11 +0800

    更新說明按鈕

--- commands/instruction.js
+++ commands/instruction.js
@@ -1,16 +1,29 @@
 export default async (event) => {
   try {
-    const ins = [
-      {
-        type: 'image',
-        originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/alex-litvin-MAYsdoYpGuk-unsplash.jpg?raw=true',
-        previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/alex-litvin-MAYsdoYpGuk-unsplash.jpg?raw=true'
-      },
-      {
-        type: 'text',
-        text: 'Hello, World!'
+    const ins = {
+      type: 'flex',
+      altText: '電影搜尋結果',
+      contents: {
+        type: 'carousel',
+        contents: [
+          {
+            type: 'image',
+            originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true',
+            previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094750.jpg?raw=true'
+          },
+          {
+            type: 'image',
+            originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true',
+            previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-095209.jpg?raw=true'
+          },
+          {
+            type: 'image',
+            originalContentUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true',
+            previewImageUrl: 'https://github.com/Zhuxian206/movieSearch/blob/master/images/instruction/Screenshot_20211231-094653.jpg?raw=true'
+          }
+        ]
       }
-    ]
+    }
     event.reply(ins)
   } catch (error) {
     console.log(error)
